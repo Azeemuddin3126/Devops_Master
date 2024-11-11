a49732d730cd5d1b201ad904d6ebd500b6573e66
Azeemuddin_shaik <52121693+Azeemuddin3126@users.noreply.github.com>
2024-11-11 13:51:49 +0530

    test2

--- gitautomate.sh
+++ gitautomate.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 echo adding files.......
-sleep 2
+sleep 1
 
 
 git add .
 
-read -p "Enter a commit msg" cmt_msg
+read -p "Enter a commit msg: " cmt_msg 
 
 git commit -m $cmt_msg
 
+echo comitted
+
 git push
+
+echo pushing files to repo
